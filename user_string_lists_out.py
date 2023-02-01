de5cbac049e72206d7ecc0b7a2c2be01b7f6b646
LevLow <levilowther@gmail.com>
2023-01-31 23:50:34 -0600

    Final push

---
+++ user_string_lists_out.py
@@ -0,0 +1,27 @@
+(base) PS C:\Users\levil\Documents\datafun-03-datatypes> & C:/Users/levil/AppData/Local/Programs/Python/Python310/python.exe c:/Users/levil/Documents/datafun-03-datatypes/user_string_lists.py
+
+String Lists 1. Using Python Built-in functions
+
+Here are the number of artists we have to choose from: 6
+
+Here are the verbs we can choose from: {'painted', 'drew', 'carved', 'sculpted'}
+
+Here is the tuple of colors and nouns: (('red', 'paintings'), ('green', 'sculptures'), ('blue', 'carvings'), ('yellow', 'drawings'))
+
+
+
+String Lists 2. Random Choice
+
+Here is a random artists from the list: Salvidor
+
+Here is a random, and probably factually incorrect, art sentence: Pablo drew tiny red paintings.
+
+
+
+String Lists 3. Get Unique Words
+
+The number of words in Shakespear's play Hamlet is: 31999.
+
+The number of unique words in Shakespear's play Hamlet is: 7714. I would say that is an impressive list of words!
+
+(base) PS C:\Users\levil\Documents\datafun-03-datatypes> 
